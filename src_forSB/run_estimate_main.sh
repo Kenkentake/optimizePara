b5e683bfea6fdc91456f4b2f8b905d8a9166306f
Kenkentake <keeeentake0908@gmail.com>
2021-06-14 22:08:51 +0900

    Modify run_estimate_main.sh

--- src_forSB/run_estimate_main.sh
+++ src_forSB/run_estimate_main.sh
@@ -7,9 +7,8 @@ mu=8
 spawn_numprocs=2
 maxevals=4
 num_of_nrn_procs=4
-exec_prog="../hocfile_forSB/x86_68/special"
+exec_prog="../hocfile_forSB/x86_64/special"
 dim_con_mat=2
 connection_data="../data/conMat.txt"
-range_filename="../data/params.txt"
+range_filename="../data/params_6.txt"
 mpirun -np 1 ./estimate_main ${lambda} ${mu} ${spawn_numprocs} ${maxevals} ${num_of_nrn_procs} ${exec_prog} ${dim_con_mat} ${connection_data} ${range_filename} 
-# mpirun -np 2 -output-filename output_mpirun.txt ./estimate_main ${lambda}
