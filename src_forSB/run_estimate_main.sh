7757d3e01b277842e5e8a700b6a404b1f67f8c80
Kenkentake <keeeentake0908@gmail.com>
2021-08-15 17:26:25 +0900

    Modify run_estimate_main.sh for 86 cells

--- src_forSB/run_estimate_main.sh
+++ src_forSB/run_estimate_main.sh
@@ -2,13 +2,14 @@
 make clean
 rm estimate_main
 make
-lambda=16
-mu=8
-spawn_numprocs=2
-maxevals=4
-num_of_nrn_procs=4
+mpicc -O3 -o make_neuro_spawn make_neuro_spawn.c
+lambda=4
+mu=2
+spawn_numprocs=4
+maxevals=15
+num_of_nrn_procs=86
 exec_prog="../hocfile_forSB/x86_64/special"
-dim_con_mat=2
+dim_con_mat=86
 connection_data="../data/conMat.txt"
-range_filename="../data/params_6.txt"
-mpirun -np 1 ./estimate_main ${lambda} ${mu} ${spawn_numprocs} ${maxevals} ${num_of_nrn_procs} ${exec_prog} ${dim_con_mat} ${connection_data} ${range_filename} 
+range_filename="../data/params_onlyWeight.txt"
+mpirun -np 1 ./estimate_main ${lambda} ${mu} ${spawn_numprocs} ${maxevals} ${num_of_nrn_procs} ${exec_prog} ${dim_con_mat} ${connection_data} ${range_filename}
